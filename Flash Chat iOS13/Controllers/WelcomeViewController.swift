e5eb94d0a362be7704709fd8301e8ec5af60ca88
Yuxin Zhou <yuxinzhou1997@gmail.com>
2020-08-02 15:26:45 -0700

    Add Firebase, and create users

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,27 +7,30 @@
 //
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Welcome page animation
-        titleLabel.text = ""
-        var charIndex = 0.0
-        let titleText = "💬ChatApp"
-        for letter in titleText {
-            print("-")
-            print(0.1 * charIndex)
-            print(letter)
-            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex += 1
-        }
+        titleLabel.text = "💬ChatApp"
+        
+//        // Welcome page animation
+//        titleLabel.text = ""
+//        var charIndex = 0.0
+//        let titleText = "💬ChatApp"
+//        for letter in titleText {
+//            print("-")
+//            print(0.1 * charIndex)
+//            print(letter)
+//            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex += 1
+//        }
 
        
     }
